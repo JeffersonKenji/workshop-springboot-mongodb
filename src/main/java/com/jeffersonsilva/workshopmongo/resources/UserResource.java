1cf3390f39fb933d4bbb20d5ce5c91359d194953
JeffersonKenji <jepesi77@gmail.com>
2019-12-27 13:29:37 -0300

    Deleção de usuário com DELETE

--- src/main/java/com/jeffersonsilva/workshopmongo/resources/UserResource.java
+++ src/main/java/com/jeffersonsilva/workshopmongo/resources/UserResource.java
@@ -18,31 +18,37 @@ import com.jeffersonsilva.workshopmongo.dto.UserDTO;
 import com.jeffersonsilva.workshopmongo.services.UserService;
 
 @RestController
-@RequestMapping(value="/users")
+@RequestMapping(value = "/users")
 public class UserResource {
-	
+
 	@Autowired
 	private UserService service;
-	
-	@RequestMapping(method=RequestMethod.GET)
- 	public ResponseEntity<List<UserDTO>> findAll() {
-		
+
+	@RequestMapping(method = RequestMethod.GET)
+	public ResponseEntity<List<UserDTO>> findAll() {
+
 		List<User> list = service.findAll();
 		List<UserDTO> listDto = list.stream().map(y -> new UserDTO(y)).collect(Collectors.toList());
 		return ResponseEntity.ok().body(listDto);
 	}
-	
-	@RequestMapping(value = "/{id}", method=RequestMethod.GET)
+
+	@RequestMapping(value = "/{id}", method = RequestMethod.GET)
 	public ResponseEntity<UserDTO> findById(@PathVariable String id) {
 		User obj = service.findById(id);
 		return ResponseEntity.ok().body(new UserDTO(obj));
 	}
-	
-	@RequestMapping(method=RequestMethod.POST)
+
+	@RequestMapping(method = RequestMethod.POST)
 	public ResponseEntity<Void> insert(@RequestBody UserDTO objDto) {
 		User obj = service.fromDTO(objDto);
 		obj = service.insert(obj);
 		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
 		return ResponseEntity.created(uri).build();
+	}
+
+	@RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
+	public ResponseEntity<Void> delete(@PathVariable String id) {
+		service.delete(id);
+		return ResponseEntity.noContent().build();
 	}
 }
