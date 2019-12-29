f1eada7a8f5e241efbcc8350c0711546c3f479ab
JeffersonKenji <jepesi77@gmail.com>
2019-12-29 20:43:48 -0300

    Consulta simples com @Query

--- src/main/java/com/jeffersonsilva/workshopmongo/repository/PostRepository.java
+++ src/main/java/com/jeffersonsilva/workshopmongo/repository/PostRepository.java
@@ -3,12 +3,16 @@ package com.jeffersonsilva.workshopmongo.repository;
 import java.util.List;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.jeffersonsilva.workshopmongo.domain.Post;
 
 @Repository
 public interface PostRepository extends MongoRepository<Post, String> {
-	
-	List<Post> findByTitleContainingIngoreCase(String text);
+
+	@Query("{ 'title': { $regex: ?0, $options: 'i' } }")
+	List<Post> searchTitle(String text);
+
+	List<Post> findByTitleContainingIgnoreCase(String text);
 }
--- src/main/java/com/jeffersonsilva/workshopmongo/services/PostService.java
+++ src/main/java/com/jeffersonsilva/workshopmongo/services/PostService.java
@@ -16,12 +16,11 @@ public class PostService {
 	@Autowired
 	private PostRepository repo;
 
-		public Post findById(String id) {
+	public Post findById(String id) {
 		Optional<Post> obj = repo.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
 	}
-		
 	public List<Post> findByTitle(String text) {
-		return repo.findByTitleContainingIngoreCase(text);
+		return repo.searchTitle(text);
 	}
 }
